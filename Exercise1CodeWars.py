d87e44f5855b33c5a49c627743dc089879e53dc9
Luis Eduardo Estrada <internetgdl@MacBook-Air-de-Luis.local>
2017-09-28 21:17:50 -0500

    Ya pasó

--- Exercise1CodeWars.py
+++ Exercise1CodeWars.py
@@ -1,19 +1,12 @@
 def longest_consec(strarr, k):
     n = len(strarr)
-    print(k)
-    if (k == 0) or (k >= n) or (k <= 0):
-        return ""
-    element = str(max([(len(x),x) for x in strarr])[1])
-    position = strarr.index(element)
-    
-    for x in range(0, k):
-        text_concatena = ""
-        position_element = position+x+1
-        if(position_element>=n):
-            position = 0
-        position_element = (x+position)
-        print(position_element)
-        if(position_element!=position):
-            text_concatena += strarr[position_element]
-        print(text_concatena)
-    return(str(max([(len(x),x) for x in strarr])[1]+text_concatena))
+    if (k == 0) or (k > n) or (k <= 0):
+       return ""
+    list_result = []
+    items_number = len(strarr)
+    for y in range(0, items_number):
+        list2 = [x for ind, x in enumerate(strarr) if ind < k]
+        list_result.append("".join(list2))
+        strarr.pop(0)   
+        
+    return(str(max([(len(x),x) for x in list_result])[1]))
--- TestExerciseCodeWars.py
+++ TestExerciseCodeWars.py
@@ -5,15 +5,15 @@ class TestExerciseCodeWares(unittest.TestCase):
 
     def test_product(self):
         self.assertEqual(Exercise1CodeWars.longest_consec(["it","it","it", "it", "it","iti"], 6), "ititititititi")
-	self.assertSetEqual(Exercise1CodeWars.longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec([], 3), "")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
-        self.assertSetEqual(Exercise1CodeWars.longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+        self.assertEqual(Exercise1CodeWars.longest_consec([], 3), "")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+        self.assertEqual(Exercise1CodeWars.longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
 
 if __name__ == '__main__':
     suite =  unittest.TestLoader().loadTestsFromTestCase(TestExerciseCodeWares)
