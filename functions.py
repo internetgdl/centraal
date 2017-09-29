3f7bbd8f2c8621e520334bf73ddad95ad092a7b9
Luis Eduardo Estrada <internetgdl@MacBook-Air-de-Luis.local>
2017-09-28 20:23:35 -0500

    funcones

--- functions.py
+++ functions.py
@@ -1,6 +1,13 @@
 def add_by(value1,value2):
-    return (value1*value2)
+    try:
+        result = int(value1 * value2)
+        return result
+    except:
+        print("el valor no es esperado")
+        raise TypeError("hubo una bronca acá")
 
 
 def sum_by(value1,value2):
+    if(value1==1):
+        raise exc.AnotherError("El valor introducido por el usuario no es valido")
     return value1+value2
